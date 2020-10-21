65b469ac3138a8a72a645f7718d65f789a51055d
erdebaru <erdebaru529@gmail.com>
2020-10-21 22:00:35 +0900

    error handling added

--- pages/error/route.js
+++ pages/error/route.js
@@ -1,23 +1,29 @@
 const template = require('./');
+const env = require('@config/environment');
+
 module.exports = {
   use404(req, res, next){
+    console.log("use404");
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
   },
   
   error(err, req, res, next){
+    
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
+    
     let errorData = {
       status: err.status || 500,
       message: err.message,
     };
-    if(!config.isProduction){
+    if(!env.isProduction){
       errorData.stack = err.stack;
     }
+    
 
-    if(err.status !== 404) logger.error(errorData);
+    if(err.status !== 404) console.error(errorData);
     // render the error page
     //res.status(err.status || 500);
     res.marko(template, errorData);
